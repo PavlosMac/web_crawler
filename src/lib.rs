68fec22686fe2a4c26aa93892943e949a31b79a5
pavlos-mac <pavlos.macdonald-kosmidis@uk.clara.net>
2022-03-31 22:04:01 +0100

    * fix lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use std::sync::Arc;
 use tokio::sync::Mutex;
 
 mod crawler;
-use crawler::*;
+// use crawler::*;
 
 mod domain;
 use domain::*;
@@ -19,19 +19,11 @@ use errors::*;
 type BoxResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
-pub async fn run() -> BoxResult<()> {
+pub async fn run() -> Result<(), RError> {
     let args: Vec<String> = env::args().collect();
-    let mut d = Domain::new(&args).unwrap();
-    d.process_domain_links().await;
+    let mut d = Domain::new(&args)?;
+    d.process_domain_links().await?;
     let findings = d.indexables;
-    //
-    let mut c = crawler::crawl(findings).await;
+    let mut c = crawler::crawl(findings).await?;
     Ok(())
-    // let state = c.results.lock().await.clone();
-    // c.link_count = state.len();
-    //
-    // for (key, value) in state {
-    //     println!("{}: {}", key, value);
-    // }
-    // println!("{:?}", findings);
 }
